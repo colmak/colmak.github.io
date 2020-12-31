5446994d451cd4ebd4f10a7124d95e022cad6aca
Saludose <roland.van.duine2@gmail.com>
2020-12-31 18:12:04 -0500

    update

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function onClickMenu(){
 	document.getElementById("menu").classList.toggle("change");
+	// let i = 0;
+	// if(i==0) {
+	// 	setTimeout(classListToggle,300);
+	// 	i++;
+	// }
+	// if(i==2) {
+	// 	setTimeout(classListToggle, 0);
+	// i-=2;
+	// }
 	document.getElementById("nav").classList.toggle("change");
-	
+
 	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+function classListToggle() {
+	document.getElementById("nav").classList.toggle("change");
 }
