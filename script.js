73c3a900bf9d1a830326c5fa55c04f1f82e8d99e
Saludose <roland.van.duine2@gmail.com>
2021-01-03 22:40:41 -0500

    .

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function onClickMenu(){
 
 	document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+function fizzBuzz () {
+	for(i=1; i<=100; i++) {
+		if (i%3==0 && i%5==0) console.log("fizzbuzz");
+		else if(i%5==0) console.log("buzz");
+		else if(i%3==0) console.log("fizz");
+		else  console.log(i);
+	}
+}
+fizzBuzz();
