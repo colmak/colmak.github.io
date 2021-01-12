3e33a97c80ddba146490e5c169f7c297846a38f1
Saludose <roland.van.duine2@gmail.com>
2021-01-11 19:03:42 -0500

    test

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 function onClickMenu(){
 	document.getElementById("menu").classList.toggle("change");
 	document.getElementById("nav").classList.toggle("change");
-
 	document.getElementById("menu-bg").classList.toggle("change-bg");
 }
-function fizzBuzz () {
-	for(i=1; i<=100; i++) {
-		if (i%3==0 && i%5==0) console.log("fizzbuzz");
-		else if(i%5==0) console.log("buzz");
-		else if(i%3==0) console.log("fizz");
-		else  console.log(i);
-	}
-}
-fizzBuzz();
