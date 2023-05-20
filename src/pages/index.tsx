bf0182d51e0bc084b300c5775d0d07ce5ace4598
colmak <69098858+colmak@users.noreply.github.com>
2023-05-19 21:57:39 -0400

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,7 +44,7 @@ const Home: NextPage = () => {
             </Link>
           </div>
           <p className="text-2xl text-white">
-            {/* {hello.data ? hello.data.greeting : "Loading tRPC query..."} */}
+            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
           </p>
         </div>
       </main>
