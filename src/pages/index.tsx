d350ff4b38bd10a5aebb88e56d39dd42fdacdb20
colmak <69098858+colmak@users.noreply.github.com>
2023-05-29 18:11:47 -0400

    Home page updated to use the new components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+// Home.tsx
 import type { NextPage } from "next";
-import { FaHome, FaBars } from "react-icons/fa";
 import { useState, useEffect, useRef } from "react";
-import Link from "next/link";
+import Header from "../components/Header";
+import Menu from "../components/Menu";
+
 
 const Home: NextPage = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -48,90 +50,12 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <header className="sticky top-0 flex items-center justify-between bg-ebony-clay-900 py-4 px-8">
-        <div>
-          <Link 
-            title="icon"
-            href="/"
-            className="flex items-center text-2xl font-semibold text-white"
-          >
-            <FaHome className="mr-2" />
-          </Link>
-        </div>
-        <nav className="hidden gap-6 text-lg font-semibold lg:flex">
-          {/* Rest of the menu options */}
-          <a href="#about" className="text-white hover:text-gray-300">
-            About
-          </a>
-          <a href="#projects" className="text-white hover:text-gray-300">
-            Projects
-          </a>
-          <a href="#contact" className="text-white hover:text-gray-300">
-            Contact
-          </a>
-          <a href="/Blog" className="text-white hover:text-gray-300">
-            Blog
-          </a>
-        </nav>
-        <div className="lg:hidden">
-          <button
-            title="button"
-            onClick={toggleMenu}
-            className="text-white hover:text-gray-300 focus:outline-none"
-            ref={buttonRef}
-          >
-            <FaBars className="h-6 w-6" />
-          </button>
-        </div>
-      </header>
-      {isMenuOpen && (
-        <div
-          className="fixed inset-0 bg-gray-900 opacity-75"
-          onClick={closeMenu}
-        />
-      )}
-      <div
-        className={`fixed inset-y-0 left-0 flex w-64 transform flex-col bg-ebony-clay-900 ${
-          isMenuOpen ? "translate-x-0" : "-translate-x-full"
-        } transition-transform duration-300 ease-in-out`}
-        ref={menuRef}
-      >
-        <a
-          href="#about"
-          className=" menu-option bg-ebony-clay-800 text-lg font-semibold text-white hover:bg-ebony-clay-800"
-          onClick={handleItemClick}
-        >
-          Home
-        </a>
-        <a
-          href="#about"
-          className=" menu-option text-lg font-semibold text-white hover:bg-ebony-clay-800"
-          onClick={handleItemClick}
-        >
-          About
-        </a>
-        <a
-          href="#projects"
-          className="menu-option text-lg font-semibold text-white hover:bg-ebony-clay-800"
-          onClick={handleItemClick}
-        >
-          Projects
-        </a>
-        <a
-          href="#contact"
-          className="menu-option text-lg font-semibold text-white hover:bg-ebony-clay-800"
-          onClick={handleItemClick}
-        >
-          Contact
-        </a>
-        <a
-          href="/Blog"
-          className="menu-option text-lg font-semibold text-white hover:bg-ebony-clay-800"
-          onClick={handleItemClick}
-        >
-          Blog
-        </a>
-      </div>
+      <Header toggleMenu={toggleMenu} />
+      <Menu
+        isMenuOpen={isMenuOpen}
+        closeMenu={closeMenu}
+        handleItemClick={handleItemClick}
+      />
 
       <main className="flex min-h-screen flex-col items-center justify-center bg-ebony-clay-950">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
