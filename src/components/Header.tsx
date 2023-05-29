894142057c22fd11cc9abe7b1a40bf8591535dd4
colmak <69098858+colmak@users.noreply.github.com>
2023-05-29 18:11:09 -0400

    Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,49 @@
+// Header.tsx
+import { FaHome, FaBars } from "react-icons/fa";
+import Link from "next/link";
+
+interface HeaderProps {
+  toggleMenu: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ toggleMenu }) => {
+  return (
+    <header className="sticky top-0 flex items-center justify-between bg-ebony-clay-900 py-4 px-8">
+      <div>
+        <Link
+          title="icon"
+          href="/"
+          className="flex items-center text-2xl font-semibold text-white"
+        >
+          <FaHome className="mr-2" />
+        </Link>
+      </div>
+      <nav className="hidden gap-6 text-lg font-semibold lg:flex">
+        {/* Rest of the menu options */}
+        <a href="#about" className="text-white hover:text-gray-300">
+          About
+        </a>
+        <a href="#projects" className="text-white hover:text-gray-300">
+          Projects
+        </a>
+        <a href="#contact" className="text-white hover:text-gray-300">
+          Contact
+        </a>
+        <a href="/Blog" className="text-white hover:text-gray-300">
+          Blog
+        </a>
+      </nav>
+      <div className="lg:hidden">
+        <button
+          title="button"
+          onClick={toggleMenu}
+          className="text-white hover:text-gray-300 focus:outline-none"
+        >
+          <FaBars className="h-6 w-6" />
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
