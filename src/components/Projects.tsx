d72d90b9ee20232495065949552f660984391bc9
colmak <69098858+colmak@users.noreply.github.com>
2023-06-01 17:16:11 -0400

    project section

---
+++ src/components/Projects.tsx
@@ -0,0 +1,67 @@
+// Projects.tsx
+import React from "react";
+import Image from "next/image";
+
+const Projects: React.FC = () => {
+  return (
+    <section id="projects" className="bg-gray-800 py-16">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-bold text-white mb-8">Projects</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {/* Project Card 1 */}
+          <div className="bg-white p-4 rounded shadow">
+            <div className="mb-4">
+              <Image
+                src="/images/project1.png" // Replace with the path to your project image
+                alt="Project 1"
+                width={300}
+                height={200}
+              />
+            </div>
+            <h3 className="text-lg font-semibold mb-2">Project 1</h3>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu
+              ante vel nulla laoreet vehicula.
+            </p>
+          </div>
+
+          {/* Project Card 2 */}
+          <div className="bg-white p-4 rounded shadow">
+            <div className="mb-4">
+              <Image
+                src="/images/project2.png" // Replace with the path to your project image
+                alt="Project 2"
+                width={300}
+                height={200}
+              />
+            </div>
+            <h3 className="text-lg font-semibold mb-2">Project 2</h3>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu
+              ante vel nulla laoreet vehicula.
+            </p>
+          </div>
+
+          {/* Project Card 3 */}
+          <div className="bg-white p-4 rounded shadow">
+            <div className="mb-4">
+              <Image
+                src="/images/project3.png" // Replace with the path to your project image
+                alt="Project 3"
+                width={300}
+                height={200}
+              />
+            </div>
+            <h3 className="text-lg font-semibold mb-2">Project 3</h3>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu
+              ante vel nulla laoreet vehicula.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect, useRef } from "react";
 import Header from "../components/Header";
 import Menu from "../components/Menu";
 
+import Projects from "../components/Projects";
 
 const Home: NextPage = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -59,11 +60,23 @@ const Home: NextPage = () => {
 
       <main className="flex min-h-screen flex-col items-center justify-center bg-ebony-clay-950">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Roland <span className="text-mandy-500">Van</span> Duine
-          </h1>
-          <p className="text-2xl text-white">WIP</p>
+          <div className="flex items-center justify-center">
+            <div>
+              <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+                Roland <span className="text-mandy-500">Van</span> Duine
+              </h1>
+              <p className="text-2xl text-white">Brief introduction goes here</p>
+            </div>
+            {/* <Image 
+              src="/images/rvdpic.png" // Replace with the path to your image
+              alt="Roland Van Duine"
+              className="w-64 h-64 m-"
+              width={1274}
+              height={660}
+            /> */}
+          </div>
         </div>
+        <Projects /> 
       </main>
     </>
   );
