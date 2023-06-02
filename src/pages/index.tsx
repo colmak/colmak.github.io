f174a77b02bf8513cb6f15178d280e9a3578e359
colmak <69098858+colmak@users.noreply.github.com>
2023-06-02 16:46:17 -0400

    small about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,39 +61,35 @@ const Home: NextPage = () => {
       />
 
       <main
-        className="flex min-h-screen flex-col items-center justify-center bg-ebony-clay-950"
+        className="flex min-h-screen items-start justify-center bg-ebony-clay-950"
         style={{
           backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('/images/background.jpg')",
           backgroundSize: "cover",
           backgroundPosition: "center"
         }}
       >
-        <div className="container flex flex-col items-start justify-center gap-12 px-4 py-16">
-          <div className="flex items-center justify-center">
-            <div>
-              <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-                Hi, I'm <span className="text-mandy-500">Roland</span>.
-              </h1>
-              <p className="text-2xl text-white">
-                I'm a multi-disciplinary professional bridging engineering, business, and the humanities.
-              </p>
-              <p className="text-2xl text-white">
-                Together, let's shape a future where technology optimizes experiences, empowers innovation, and drives collective success.
-              </p>
-              <p className="text-2xl text-white">
-                Embrace the challenge, work hard, and watch your dreams transform into reality.
-              </p>
-            </div>
-            {/* <Image 
-              src="/images/rvdpic.png" // Replace with the path to your image
-              alt="Roland Van Duine"
-              className="w-64 h-64"
-              width={1274}
-              height={660}
-            /> */}
+        <div className="container flex items-center justify-start gap-12 px-4 py-16 ">
+          <div className="w-1/2">
+            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+              Hi, I'm <span className="text-mandy-500">Roland</span>.
+            </h1>
+            <p className="text-2xl text-white">
+            I bridge engineering, business, and the humanities, shaping a future where technology optimizes experiences, empowers innovation, and drives collective success.            </p>
+
+            <p className="text-2xl text-white">
+              Embrace the challenge, work hard, and watch your dreams transform into reality.
+            </p>
           </div>
+          {/* <Image 
+            src="/images/rvdpic.png" // Replace with the path to your image
+            alt="Roland Van Duine"
+            className="w-64 h-64"
+            width={1274}
+            height={660}
+          /> */}
         </div>
       </main>
+
 
       <Separator text="Projects"/>
       <Projects /> 
