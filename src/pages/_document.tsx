6f267a63d06807e0b1fc6e981aff54b81985cf7a
colmak <69098858+colmak@users.noreply.github.com>
2023-06-26 17:32:03 -0400

    preview update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
-// _document.js
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta property="og:image" content="/images/background.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
       <body className="bg-ebony-clay-950">
         <Main />
         <NextScript />
