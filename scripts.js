c2ecb07df1920d6bf64b6274aa2ddacbfff65d05
Roland Van Duine <69098858+colmak@users.noreply.github.com>
2023-10-30 13:01:35 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll(".animated-section");
-
-    sections.forEach((section, index) => {
-        setTimeout(() => {
-            section.classList.add("show");
-        }, 500 * index); // Delay each section's animation
-    });
-});
