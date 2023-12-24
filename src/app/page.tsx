fe6aeab1183b80eb89beafed6b026cb31aa29abc
colmak <69098858+colmak@users.noreply.github.com>
2023-12-24 01:17:36 -0500

    New changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,21 +11,21 @@ export default function HomePage() {
   };
 
   return (
-    <div className={`flex flex-col min-h-screen ${isDarkMode ? 'bg-black' : 'bg-white'} text-black`}>
+    <div className={`flex flex-col min-h-screen ${isDarkMode ? 'bg-black' : 'bg-white'} text-${isDarkMode ? 'white' : 'black'}`}>
       {/* Header */}
-      <header className="p-4 bg-gray-200">
+      <header className={`p-4 ${isDarkMode ? 'bg-gray-800' : 'bg-gray-200'}`}>
         <div className="container flex justify-between items-center">
           {/* Home Link */}
           <Link href="/">
-            <a className="text-xl font-semibold">Home</a>
+            <a className={`text-xl font-semibold ${isDarkMode ? 'text-white' : 'text-black'}`}>Home</a>
           </Link>
           {/* Demo Links */}
           <div className="flex gap-4">
             <Link href="https://example.com/demo1">
-              <a className="hover:text-blue-500">Demo 1</a>
+              <a className={`hover:text-${isDarkMode ? 'blue-300' : 'blue-500'}`}>Demo 1</a>
             </Link>
             <Link href="https://example.com/demo2">
-              <a className="hover:text-blue-500">Demo 2</a>
+              <a className={`hover:text-${isDarkMode ? 'blue-300' : 'blue-500'}`}>Demo 2</a>
             </Link>
           </div>
         </div>
@@ -37,31 +37,6 @@ export default function HomePage() {
           Create <span className={`text-${isDarkMode ? 'gray' : 'purple-500'}`}>T3</span> App
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className={`flex max-w-xs flex-col gap-4 rounded-xl ${
-              isDarkMode ? 'bg-white/10' : 'bg-black/10'
-            } p-4 text-white hover:${isDarkMode ? 'bg-white/20' : 'bg-black/20'}`}
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className={`flex max-w-xs flex-col gap-4 rounded-xl ${
-              isDarkMode ? 'bg-white/10' : 'bg-black/10'
-            } p-4 text-white hover:${isDarkMode ? 'bg-white/20' : 'bg-black/20'}`}
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to deploy it.
-            </div>
-          </Link>
         </div>
         <button
           className={`p-2 mt-4 rounded ${
