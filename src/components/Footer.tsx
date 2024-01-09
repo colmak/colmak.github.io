8243b38e21f2aa92333d154046e5f5a7dbbeeffb
colmak <69098858+colmak@users.noreply.github.com>
2024-01-09 17:39:07 -0500

    Update footer copyright information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="w-full text-center border-t border-grey p-4 pin-b">
       <p>
-        © {year} Roland Van Duine. All rights reserved.
+        &copy; {year} Your Name. All rights reserved.
       </p>
     </footer>
   );
