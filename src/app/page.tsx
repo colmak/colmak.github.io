271629151caf8f6b70e2a808a37c0f9921940932
colmak <69098858+colmak@users.noreply.github.com>
2024-01-08 23:25:20 -0500

    Add social media links to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { IoMdSunny, IoMdMoon } from "react-icons/io";
 import Link from "next/link";
 import UnderlinedText from "~/components/UnderlinedText";
-
 
 export default function HomePage() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -33,13 +33,13 @@ export default function HomePage() {
           </Link>
           <div className="flex gap-4">
             <Link
-              href="https://example.com/demo1"
+              href="/projects"
               className="text-gray-500 hover:text-black dark:text-gray-300 dark:hover:text-white"
             >
               Projects
             </Link>
             <Link
-              href="https://example.com/demo2"
+              href="https://linkedin.com/in/rolandvanduine" target="_blank" rel="noopener noreferrer"
               className="text-gray-500 hover:text-black dark:text-gray-300 dark:hover:text-white"
             >
               Contact
@@ -50,13 +50,12 @@ export default function HomePage() {
             >
               {isDarkMode ? <IoMdSunny /> : <IoMdMoon />}
             </button>
-            
           </div>
         </div>
       </header>
 
       <div className="container mx-auto flex items-center justify-center">
-        <main className="container flex max-w-screen-sm flex-col items-start justify-start gap-4 px-4 py-16">
+        <main className="container flex max-w-screen-sm flex-col items-start justify-start gap-4 px-4 py-8">
           <h1 className="pb-3 text-[2rem] font-bold tracking-tight text-black dark:text-white">
             Roland Van Duine
           </h1>
@@ -76,15 +75,36 @@ export default function HomePage() {
             </div>
           </div>
           <p className="text-gray-500 dark:text-gray-400">
-          My passion lies in taking ideas and turning 
-          them into reality. You can explore my <UnderlinedText href="/projects">complete list of projects here</UnderlinedText>.
+            My passion lies in taking ideas and turning them into reality. You
+            can explore my{" "}
+            <UnderlinedText href="/projects">
+              complete list of projects here
+            </UnderlinedText>
+            . Also check out my{" "}
+            <UnderlinedText href="https://github.com/colmak/">
+              github
+            </UnderlinedText>{" "}
+            for other projects
           </p>
           <p className="text-gray-500 dark:text-gray-400">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam
-            suscipit ipsa voluptatum reiciendis est veritatis omnis eos
-            dignissimos odit excepturi quisquam voluptatibus repellendus eveniet
-            eum maxime quos, accusamus deserunt ullam.
+            When I'm not coding, I love rock climbing and exploring new places.
+            If you're ever near CT, USA we could possibly meet up.
           </p>
+
+          <div className="mt-8 flex gap-4">
+            <UnderlinedText href="https://github.com/colmak">
+              <div className="flex items-center">
+                <FaGithub className="mr-2" />
+                GitHub
+              </div>
+            </UnderlinedText>
+            <UnderlinedText href="https://linkedin.com/in/rolandvanduine">
+              <div className="flex items-center">
+                <FaLinkedin className="mr-2" />
+                LinkedIn
+              </div>
+            </UnderlinedText>
+          </div>
         </main>
       </div>
     </div>
