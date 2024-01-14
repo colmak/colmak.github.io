7d82d895fb82b4f2322daa6600abb357a004a4fa
colmak <69098858+colmak@users.noreply.github.com>
2024-01-13 23:44:41 -0500

    Update UnderlinedTextWithIcon component styling

--- src/components/UnderlinedTextWithIcon.tsx
+++ src/components/UnderlinedTextWithIcon.tsx
@@ -8,8 +8,8 @@ interface UnderlinedTextProps {
 }
 
 const UnderlinedTextWithIcon = ({ children, href, Icon }: UnderlinedTextProps) => (
-  <a href={href} target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-500 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors duration-600 cursor-pointer">
-    <Icon className="mr-2 text-gray-500 dark:text-gray-400 hover:text-black dark:hover:text-white" />
+  <a href={href} target="_blank" rel="noopener noreferrer" className="flex text-gray-500 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors duration-600 cursor-pointer">
+    <Icon className="text-2xl mr-4 text-gray-500 dark:text-gray-400 hover:text-black dark:hover:text-white" />
     {children}
   </a>
 );
