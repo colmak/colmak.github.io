41f6ef4cb38200a3c4d1f5b18310c5e88899d1b8
colmak <69098858+colmak@users.noreply.github.com>
2024-01-13 23:44:44 -0500

    Refactor project page to use ProjectCard component

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -7,6 +7,7 @@ import Head from "next/head";
 import Link from "next/link";
 import Footer from "~/components/Footer";
 import UnderlinedTextWithIcon from "~/components/UnderlinedTextWithIcon";
+import ProjectCard from "~/components/ProjectCardProps";
 
 export default function ProjectPage() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -63,76 +64,44 @@ export default function ProjectPage() {
         </header>
 
         <div className="container mx-auto flex items-center justify-center">
-          <main className="slide-enter-content container flex max-w-screen-sm flex-col items-start justify-start gap-4 px-4 py-8">
+          <main className="slide-enter-content 12 container flex max-w-screen-sm flex-col items-start justify-start gap-4 px-8 py-12">
             <h1 className="w-full pb-3 text-center text-[2rem] font-bold tracking-tight text-black dark:text-white">
               Projects
             </h1>
-            <div className="slide-enter-content grid w-full grid-cols-2 gap-4">
-              <div className="gap-2">
-                <div>
-                  <UnderlinedTextWithIcon
-                    Icon={FaGithub}
-                    href="https://github.com/CCSU-Computer-Science-Club/ccsu-cs-club-website"
-                  >
-                    CCSU CS Club Website
-                  </UnderlinedTextWithIcon>
-                </div>
-                <div className="text-gray-500 dark:text-gray-400">
-                  Next.js, Tailwind.css, tRPC
-                </div>
-              </div>
-              <div className="gap-2 ">
-                <div className="">
-                  <UnderlinedTextWithIcon
-                    Icon={FaGithub}
-                    href="https://www.yusufmzaidi.com/"
-                  >
-                    Yusuf&apos;s Portfolio
-                  </UnderlinedTextWithIcon>
-                </div>
-                <div className="text-gray-500 dark:text-gray-400">
-                  Next.js, Tailwind.css, Framer Motion
-                </div>
-              </div>
-              <div className="gap-2">
-                <div>
-                  <UnderlinedTextWithIcon
-                    Icon={FaGithub}
-                    href="https://github.com/colmak/SerenityNow"
-                  >
-                    Serenity Now
-                  </UnderlinedTextWithIcon>
-                </div>
-                <div className="text-gray-500 dark:text-gray-400">
-                  Python, Gradio, OpenAI GPT-3.5
-                </div>
-              </div>
-              <div className="gap-2">
-                <div>
-                  <UnderlinedTextWithIcon
-                    Icon={FaGithub}
-                    href="https://github.com/BigRedDoge/GreenSwitch"
-                  >
-                    Green Switch
-                  </UnderlinedTextWithIcon>
-                </div>
-                <div className="text-gray-500 dark:text-gray-400">
-                  React Native, Flask, SQLite
-                </div>
-              </div>
-              <div className="gap-2">
-                <div>
-                  <UnderlinedTextWithIcon
-                    Icon={FaGithub}
-                    href="https://github.com/colmak/Weathered-Weather-App"
-                  >
-                    Weathered
-                  </UnderlinedTextWithIcon>
-                </div>
-                <div className="text-gray-500 dark:text-gray-400">
-                  Python, Django, SQLite
-                </div>
-              </div>
+            <div className="slide-enter-content grid w-full grid-cols-2 gap-2">
+              <ProjectCard
+                Icon={FaGithub}
+                href="https://github.com/CCSU-Computer-Science-Club/ccsu-cs-club-website"
+                title="CS Club Site"
+                description="Next.js, Tailwind.css, tRPC"
+              />
+              <ProjectCard
+                Icon={FaGithub}
+                href="https://www.yusufmzaidi.com/"
+                title="Yusuf's Portfolio"
+                description="Next.js, Tailwind.css, Framer Motion"
+              />
+
+              <ProjectCard
+                Icon={FaGithub}
+                href="https://github.com/colmak/SerenityNow"
+                title="Serenity Now"
+                description="Python, Gradio, OpenAI GPT-3.5"
+              />
+
+              <ProjectCard
+                Icon={FaGithub}
+                href="https://github.com/BigRedDoge/GreenSwitch"
+                title="Green Switch"
+                description="React Native, Flask, SQLite"
+              />
+
+              <ProjectCard
+                Icon={FaGithub}
+                href="https://github.com/colmak/Weathered-Weather-App"
+                title="Weathered"
+                description="Python, Django, SQLite"
+              />
             </div>
             <Footer></Footer>
           </main>
