083b10748df36c8377c7247a55c5e04e5f4a7781
colmak <69098858+colmak@users.noreply.github.com>
2024-01-19 22:24:42 -0500

    Update hover text color in ProjectCardProps.tsx

--- src/components/ProjectCardProps.tsx
+++ src/components/ProjectCardProps.tsx
@@ -13,7 +13,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ Icon, href, title, descriptio
       <div className="flex flex-col gap-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded transition-colors duration-200 p-2">
         <div className="flex-grow flex items-center text-gray-500 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors duration-600 cursor-pointer">
           <Icon className="text-2xl mr-4 text-gray-500 dark:text-gray-400 hover:text-black dark:hover:text-white" />
-          <span className="hover:text-black">{title}</span>
+          <span className="hover:text-black dark:hover:text-white">{title}</span>
         </div>
         <div className="text-[.93rem] text-gray-500 dark:text-gray-400">
           {description}
