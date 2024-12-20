c586de45035dd53125993ba1e6314fbb91f06bbe
colmak <69098858+colmak@users.noreply.github.com>
2024-12-19 19:50:24 -0500

    remove cheats

--- src/app/wordle/page.tsx
+++ src/app/wordle/page.tsx
@@ -62,7 +62,6 @@ export default function WordlePage() {
       randomWord = commonWords[randomIndex]?.toUpperCase() ?? "APPLE";
 
       setTargetWord(randomWord);
-      console.log(randomWord)
     }
   }, [commonWords]);
 
