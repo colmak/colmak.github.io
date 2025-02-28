04add9c616848e505eb7a2ae198a8e499c5ebafd
Roland Van Duine <69098858+colmak@users.noreply.github.com>
2025-02-28 11:50:58 -0500

    Refactor homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,14 @@
 "use client";
 
-import Cookies from "js-cookie";
-import { useState, useEffect } from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { IoMdSunny, IoMdMoon } from "react-icons/io";
-import Link from "next/link";
-import UnderlinedText from "~/components/UnderlinedText";
 import Footer from "~/components/Footer";
+import Header from "~/components/Header";
+import UnderlinedText from "~/components/UnderlinedText";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function HomePage() {
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(() => {
-    const cookie = Cookies.get("darkMode");
-    return cookie ? (JSON.parse(cookie) as boolean) : false;
-  });
-
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-    Cookies.set("darkMode", String(!isDarkMode));
-  };
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [isDarkMode]);
-
   return (
     <div className="flex min-h-screen flex-col bg-white text-gray-500 dark:bg-black dark:text-gray-200">
-      <header className="flex items-center justify-center p-4">
-        <div className="container flex items-center justify-between">
-          <Link
-            href="/"
-            className="text-xl font-semibold text-black dark:text-white"
-          >
-            RVD
-          </Link>
-          <div className="flex gap-4">
-            <Link
-              href="/projects"
-              className="text-gray-500 hover:text-black dark:text-gray-300 dark:hover:text-white"
-            >
-              Projects
-            </Link>
-            <Link
-              href="https://linkedin.com/in/rolandvanduine"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-500 hover:text-black dark:text-gray-300 dark:hover:text-white"
-            >
-              Contact
-            </Link>
-            <button
-              className="text-gray-500 hover:text-black dark:text-gray-300 dark:hover:text-white"
-              onClick={toggleTheme}
-            >
-              {isDarkMode ? <IoMdSunny /> : <IoMdMoon />}
-            </button>
-          </div>
-        </div>
-      </header>
-
+      <Header />
       <div className="container mx-auto flex items-center justify-center">
         <main className="slide-enter-content container flex max-w-screen-sm flex-col items-start justify-start gap-4 px-4 py-12">
           <h1 className="pb-3 text-[2rem] font-bold tracking-tight text-black dark:text-white">
@@ -89,8 +36,7 @@ export default function HomePage() {
             for other projects
           </p>
           <p className="text-gray-500 dark:text-gray-400">
-            When I&apos;m not coding, I love rock climbing, learning languages and exploring new
-            places.
+            When I&apos;m not coding, I love rock climbing, learning languages and exploring new places.
           </p>
 
           <div className="mt-8 flex gap-4">
