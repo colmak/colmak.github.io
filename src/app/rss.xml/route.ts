812c0e3718bc766a222cff5b8b7cc342420c5ec4
colmak <69098858+colmak@users.noreply.github.com>
2025-03-18 08:51:28 -0400

    Refactor GET function in RSS route to remove async keyword for improved simplicity

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -1,7 +1,7 @@
 import { getAllPosts } from "~/lib/mdx";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export function GET() {
   const posts = getAllPosts();
   const baseUrl =
     process.env.NEXT_PUBLIC_BASE_URL || "https://rolandvanduine.com";
