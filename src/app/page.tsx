2d136045c8bfb35c8dcefffca43b535919590151
Roland Van Duine <69098858+colmak@users.noreply.github.com>
2025-03-25 17:47:01 -0400

    Refactor layout structure for HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,60 +16,63 @@ export default function HomePage() {
   return (
     <div className="flex min-h-screen flex-col bg-white text-gray-500 dark:bg-black dark:text-gray-200">
       <Header />
-      <main className="container mx-auto flex max-w-screen-sm flex-col items-start justify-center gap-4 px-4 py-12 slide-enter-content">
-        <h1 className="pb-3 text-2xl font-bold tracking-tight text-black dark:text-white">
-          Roland Van Duine
-        </h1>
-        <p className="text-gray-500 dark:text-gray-400">
-          Hey, I am Roland Van Duine, a software engineer.
-        </p>
-        <p className="text-gray-500 dark:text-gray-400">
-          Software Engineer at{" "}
-          <UnderlinedText href="https://www.travelers.com/">
-            Travelers
-          </UnderlinedText>
-        </p>
-        <p className="text-gray-500 dark:text-gray-400">
-          I like to code. You can explore my{" "}
-          <UnderlinedText href="/projects">projects here</UnderlinedText>. Also,
-          check out my{" "}
-          <UnderlinedText href="https://github.com/colmak/">
-            github
-          </UnderlinedText>{" "}
-          for other projects.
-        </p>
-        <p className="text-gray-500 dark:text-gray-400">
-          Some hobbies are rock climbing, language learning, reading, and
-          exploring new places.
-        </p>
+      <div className="container mx-auto flex items-center justify-center">
+        <main className="slide-enter-content container flex max-w-screen-sm flex-col items-start justify-center gap-4 px-4 py-12">
+          <h1 className="pb-3 text-2xl font-bold tracking-tight text-black dark:text-white">
+            Roland Van Duine
+          </h1>
+          <p className="text-gray-500 dark:text-gray-400">
+            Hey, I am Roland Van Duine, a software engineer.
+          </p>
+          <p className="text-gray-500 dark:text-gray-400">
+            Software Engineer at{" "}
+            <UnderlinedText href="https://www.travelers.com/">
+              Travelers
+            </UnderlinedText>
+          </p>
+          <p className="text-gray-500 dark:text-gray-400">
+            I like to code. You can explore my{" "}
+            <UnderlinedText href="/projects">projects here</UnderlinedText>. Also,
+            check out my{" "}
+            <UnderlinedText href="https://github.com/colmak/">
+              github
+            </UnderlinedText>{" "}
+            for other projects.
+          </p>
+          <p className="text-gray-500 dark:text-gray-400">
+            Some hobbies are rock climbing, language learning, reading, and
+            exploring new places.
+          </p>
 
-        {latestPosts.length > 0 && (
-          <div className="mt-4 w-full text-center slide-enter-content">
-            <h2 className="mb-2 text-xl text-black dark:text-white">Writing</h2>
-            <ul className="space-y-1">
-              {latestPosts.map((post) => (
-                <li key={post.slug}>
-                  <InternalBlogLink
-                    href={`/blog/${post.slug}`}
-                    className="no-underline transition-colors duration-200 hover:text-black dark:hover:text-white"
-                  >
-                    {post.title}
-                  </InternalBlogLink>
-                </li>
-              ))}
-            </ul>
-            <div className="mt-2">
-              <InternalBlogLink
-                href="/blog"
-                className="no-underline transition-colors duration-200 hover:text-black dark:hover:text-white"
-              >
-                View all posts
-              </InternalBlogLink>
+          {latestPosts.length > 0 && (
+            <div className="mt-4 w-full text-center">
+              <h2 className="mb-2 text-xl text-black dark:text-white">Writing</h2>
+              <ul className="space-y-1">
+                {latestPosts.map((post) => (
+                  <li key={post.slug}>
+                    <InternalBlogLink
+                      href={`/blog/${post.slug}`}
+                      className="no-underline transition-colors duration-200 hover:text-black dark:hover:text-white"
+                    >
+                      {post.title}
+                    </InternalBlogLink>
+                  </li>
+                ))}
+              </ul>
+              <div className="mt-2">
+                <InternalBlogLink
+                  href="/blog"
+                  className="no-underline transition-colors duration-200 hover:text-black dark:hover:text-white"
+                >
+                  View all posts
+                </InternalBlogLink>
+              </div>
             </div>
-          </div>
-        )}
-      </main>
-      <Footer />
+          )}
+          <div className="pb-10"></div>
+          <Footer/>
+        </main>
+      </div>
     </div>
   );
 }
